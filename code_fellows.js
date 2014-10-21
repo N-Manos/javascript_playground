8104174b0cad6ce0d95aa2443b1bc1f4a22edea8
NickManos <manos.nick@gmail.com>
2014-10-21 10:35:48 -0700

    Refactoring

--- code_fellows.js
+++ code_fellows.js
@@ -1,25 +1,26 @@
-function codeFellows (address) {
+function CodeFellows (address) {
   this.address = address;
   this.motto = "Learn more faster.";
   this.founded = 2013;
   this.students = {};
-  this.addStudent = addStudent;
-  this.toString = toString
-
-  function addStudent (name, info) {
-    this.students[name] = info
-  }
-
-  function toString () {
-    return Object.keys(this)
-  }
-
+}
+ 
+CodeFellows.prototype.addStudent = function(name, info) {
+  this.students[name] = info
 };
-
-var newBranch = new codeFellows("511 Boren Ave N, Seattle, WA 98109");
-
+ 
+CodeFellows.prototype.toString = function() {
+  for (var property in this) {
+    if (this.hasOwnProperty(property)) {
+      console.log(property + ": " + this[property]);
+    }
+  }
+};
+ 
+var newBranch = new CodeFellows("511 Boren Ave N, Seattle, WA 98109");
+ 
 newBranch.addStudent('Amy', {age: 27, program: 'rails dev accelerator', career: 'web dev'})
 newBranch.addStudent('Ben', {age: 43, program: 'UX dev accelerator', career: 'UX design'})
-
+ 
 console.log(newBranch.students)
 console.log(newBranch.toString())
